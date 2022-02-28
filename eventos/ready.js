b6cf787a8da9f1b6bca0eb4c73180eaeab97ab23
JeffinBR <100450049+JeffinBR@users.noreply.github.com>
2022-02-28 07:40:32 -0300

    Update ready.js

--- eventos/ready.js
+++ eventos/ready.js
@@ -1,1 +1,28 @@
 
+const client = require("../index");
+client.on('ready', () => {
+
+    
+      process.on('unhandledRejection', (reason, p) => {
+        console.log('SCRIPT REJEITADO');
+        console.log(reason, p)
+    });
+    
+    process.on('uncaughtExceptionMonitor', (err, origin) => {
+        console.log('BLOQUEADO');
+        console.log(err, origin)
+    });
+    
+    process.on('multipleResolves', (type, promise, reason) => {
+        console.log('VÁRIOS ERROS');
+        console.log(type, promise, reason)
+    });
+    
+    process.on("uncaughtException", (err, origin) => {
+        console.log('CATCH ERROR');
+        console.log(err, origin)
+    });
+
+    console.log(`${client.user.tag} Kuzin verder estou funcionando carai!`)
+
+})
